4e3ab5318d7f2233a89ba059828e7bab19bbef7b
kumar <gajmerk17@gmail.com>
2026-05-06 01:29:43 +0545

    routes and middleware setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
+// importing 'express'
 const express = require("express");
 const app = express();
+// importing 'cors'
+const cors = require("cors");
 
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
 const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
 
