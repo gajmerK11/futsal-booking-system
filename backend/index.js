2034b55c95ce7aeb840ad33f3ff6de05b8c3698e
kumar <gajmerk17@gmail.com>
2026-05-05 23:10:56 +0545

    routes setup:

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
+
 app.listen(3000, () => {
   console.log("Server started");
 });
