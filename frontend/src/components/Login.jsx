8282d00d2ebff719ecc0e124b2ef5a11db686608
kumar <gajmerk17@gmail.com>
2026-05-27 22:23:06 +0545

    login form styling

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,10 @@
 import leftSideImage from "../assets/login.jpg";
+import { FcGoogle } from "react-icons/fc";
+import { FaApple } from "react-icons/fa";
 
 function Login() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-stone-100">
       <div className="flex rounded-2xl shadow-2xl overflow-hidden w-[900px]">
         {/* Left side image */}
         <div className="w-1/2">
@@ -15,14 +17,22 @@ function Login() {
         {/* Right side form */}
         <div className="w-1/2 p-10">
           <h2 className="text-3xl font-bold">JOIN THE LEAGUE</h2>
-          <p>Sign in to your account</p>
+          <p className="mt-2">Sign in to your account</p>
           {/* OAuth buttons */}
           <div className="flex gap-3 mt-6">
             <button className="flex items-center justify-center gap-2 border border-gray-300 rounded-full px-4 py-2 w-full text-sm font-medium hover:bg-gray-50 hover:border-indigo-500 transition-colors cursor-pointer relative group">
+              <FcGoogle />
               Google
+              <span className="absolute -top-6  left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
+                Coming Soon
+              </span>
             </button>
             <button className="flex items-center justify-center gap-2 border border-gray-300 rounded-full px-4 py-2 w-full text-sm font-medium hover:bg-gray-50 hover:border-indigo-500 transition-colors cursor-pointer relative group">
+              <FaApple />
               Apple ID
+              <span className="absolute -top-6 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
+                Coming Soon
+              </span>
             </button>
           </div>
           {/* 'Or continue with email' part */}
@@ -38,20 +48,27 @@ function Login() {
             <input
               type="email"
               placeholder="Email"
-              className="border border-gray-300 rounded-full px-4 py-2 text-sm outline-none focus:border-indigo-500 transition-colors"
+              className="w-full bg-gray-100 border border-gray-200 px-4 py-3 text-sm outline-none focus:border-indigo-500 transition-colors"
             />
             <input
               type="password"
               placeholder="Password"
-              className="border border-gray-300 rounded-full px-4 py-2 text-sm outline-none focus:border-indigo-500 transition-colors"
+              className="border px-4 py-2 text-sm outline-none focus:border-indigo-500 transition-colors"
             />
             <button
               type="submit"
-              className="w-full bg-indigo-600 text-white py-2 rounded-full font-medium hover:bg-indigo-700 transition-colors cursor-pointer"
+              className="w-full bg-indigo-600 text-white py-2 font-medium hover:bg-indigo-700 transition-colors cursor-pointer"
             >
               Sign in
             </button>
           </form>
+          {/* 'Don't have account' part */}
+          <p className="text-sm text-center text-gray-500 mt-4">
+            Don't have an account?{" "}
+            <span className="text-indigo-600 font-semibold cursor-pointer">
+              Sign up
+            </span>
+          </p>
         </div>
       </div>
     </div>
