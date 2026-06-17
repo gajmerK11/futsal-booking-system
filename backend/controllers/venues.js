d00b5f6006b5bcd81ac6c155d18d7b655442260c
kumar <gajmerk17@gmail.com>
2026-06-17 22:42:53 +0545

    distance still going on

---
+++ backend/controllers/venues.js
@@ -0,0 +1,21 @@
+const pool = require("../models/db.js");
+
+async function getNearbyVenues(req, res) {
+  // Step-1: Extract 'user_id' from req.user (req.user is set by 'verifyToken' middleware)
+  const user_id = req.user.id;
+  // Step-2: DB query
+  const result = await pool.query(
+    "SELECT latitude, longitude from users WHERE user_id = $1",
+    [user_id],
+  );
+  // Extracting lat/lon from result ('pool.query' returns Result object)
+  const latitude = result.rows[0].latitude;
+  const longitude = result.rows[0].longitude;
+  // Step-3: Haversine subquery
+  const haversineExpression = `6371 * acos(
+  cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) + sin(radians($1)) * sin(radians(latitude))`;
+  const venueResult = await pool.query(
+    `SELECT * FROM (SELECT *, ${haversineExpression} AS distance_km FROM futsal_venues) AS venues_with_distance WHERE distance_km <=10 ORDER BY distance_km ASC`,
+    [latitude, longitude],
+  );
+}
