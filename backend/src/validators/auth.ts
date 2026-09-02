0c796b5185686e4a3170b8ab907d4122656ade9e
kumar <gajmerk17@gmail.com>
2026-09-02 11:00:37 +0545

    zod validation for register controller data

--- backend/src/validators/auth.ts
+++ backend/src/validators/auth.ts
@@ -1,13 +1,18 @@
 import z from "zod";
 
-const registerSchema = z.object({
-  username: z.string(),
-  email: z.email(),
-  password: z.string().min(8),
-  confirmPassword: z.string().min(8),
-  phoneNumber: z.string().regex(/^9\d{9}$/),
-  role: z.enum(["user", "owner"]),
-  location: z.string().min(1),
-  lat: z.coerce.number().min(-90).max(90),
-  lon: z.coerce.number().min(-180).max(180),
-});
+const registerSchema = z
+  .object({
+    username: z.string(),
+    email: z.email(),
+    password: z.string().min(8),
+    confirmPassword: z.string().min(8),
+    phone_number: z.string().regex(/^9\d{9}$/),
+    role: z.enum(["user", "owner"]),
+    location: z.string().min(1),
+    lat: z.coerce.number().min(-90).max(90),
+    lon: z.coerce.number().min(-180).max(180),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match", // error message shown on failure of condition
+    path: ["confirmPassword"], // which field the error attaches to
+  });
